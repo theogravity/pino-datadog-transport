7a20c08f36ab7b21e0952a37d1728664518a3d84
Daniel Hochman <danielhochman@users.noreply.github.com>
2023-11-01 15:01:03 -0500

    fix `setServerVariables is deprecated` warning (#18)

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { DDTransportOptions, processLogBuilder } from './process-logs'
 
 module.exports = function (options: DDTransportOptions) {
   const configuration = client.createConfiguration(options.ddClientConf)
-  client.setServerVariables(configuration, options?.ddServerConf || {})
+  configuration.setServerVariables(options?.ddServerConf || {})
   const apiInstance = new v2.LogsApi(configuration)
 
   return build(processLogBuilder(options, apiInstance))
